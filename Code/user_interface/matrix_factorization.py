7fef708e874636e4edc12fbb61e540d1eec717ce
ArshdeepSingh98 <arshdeep.mailme@gmail.com>
2023-04-10 10:45:19 -0700

    Code refractor

--- Code/user_interface/matrix_factorization.py
+++ Code/user_interface/matrix_factorization.py
@@ -1,10 +1,8 @@
 import sklearn
 import pandas as pd
-from sklearn.cluster import KMeans
 import pickle
 
 user_specific = pd.read_csv('user_specific.csv')
-ratings_df = pd.read_csv('netflix_filtered_ratings.csv')
 
 def load_model():
     with open('svd.pkl', 'rb') as f:
@@ -14,6 +12,7 @@ def load_model():
 def get_recommendations(user_id, user_specific):
     user_specific['Estimate_Score'] = user_specific['Movie_Id'].apply(lambda x: svd.predict(user_id, x).est)
     user_specific = user_specific.sort_values('Estimate_Score', ascending=False)
+    print(user_specific.head())
     return user_specific.head()
 
 def get_movie_recommendations(user_id):
